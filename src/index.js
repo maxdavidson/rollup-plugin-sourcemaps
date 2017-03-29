d7ca69683f865f8cbe69a89167b5349aac08eb77
Diego Vilar <diegovilar@gmail.com>
2017-03-29 10:32:49 -0300

    Fix: Paths to sourcemaps containing spaces were not being resolved
    Signed-off-by: Diego Vilar <diegovilar@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import { createFilter } from 'rollup-pluginutils';
 import { resolve as resolveSourceMap } from 'source-map-resolve';
 import { readFile } from 'fs';
 
+// resolveSourceMap will url-escape paths containing spaces, which will make
+// readFile fail to locate files. So we need to unescape paths before passing
+// them to readFile.
+function readFileProxy(...args) {
+  if (args.length) {
+    // eslint-disable-next-line no-param-reassign
+    args[0] = unescape(args[0]);
+  }
+  return readFile(...args);
+}
+
 export default function sourcemaps({ include, exclude } = {}) {
   const filter = createFilter(include, exclude);
 
@@ -19,7 +30,7 @@ export default function sourcemaps({ include, exclude } = {}) {
             // Failed reading file, let the next plugin deal with it
             resolve(null);
           } else {
-            resolveSourceMap(code, id, readFile, (err, sourceMap) => {
+            resolveSourceMap(code, id, readFileProxy, (err, sourceMap) => {
               if (err || sourceMap === null) {
                 // Either something went wrong, or there was no source map
                 resolve(code);
